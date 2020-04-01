aef69e22055bcef5114d3cecafa1cfcdf85daee0
felipesflisboa <felipesflisboa@users.noreply.github.com>
2020-03-31 21:50:18 -0300

    fix: reduce 1 line from top movement area

--- Scripts/GameConstants.ts
+++ Scripts/GameConstants.ts
@@ -6,7 +6,7 @@ namespace game {
 	export class GameConstants{
 		public static readonly SCREEN_SIZE : Vector2 = new Vector2(84, 48);
 		public static readonly GROUND_POS_Y : number = -20;
-		public static readonly Y_MOVEMENT_RANGE : ut.Math.Range = new ut.Math.Range(-3, 14);
+		public static readonly Y_MOVEMENT_RANGE : ut.Math.Range = new ut.Math.Range(-3, 13);
 		public static readonly ZONE_WIDTH : number = 256;
 		public static readonly ANIMAL_DESPAWN_MIN_DISTANCE : number = 112;
 		public static readonly PLAYER_INITIAL_EXTRA_LIVE_COUNT : number = 3;
